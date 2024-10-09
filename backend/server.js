d41823d37198c865499dfd2ee66ac15f166e2c0e
Nick <aurelius@MacBook-Pro.local>
2024-10-09 15:17:01 -0700

    Update server.js to use process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const routes = require('./routes')
-const port = 5001
+const port = process.env.PORT || 5001
 
 // Enable CORS to allow cross-origin requests from React
 app.use(cors())
@@ -10,7 +10,6 @@ app.use(cors())
 // Use the routes defined in routes.js
 app.use('/', routes)
 
-// Start the server on port 5001
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
