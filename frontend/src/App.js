c1b8d4c7290e69a204521061c67c712036c1884a
Nick <aurelius@MacBook-Pro.local>
2024-11-21 16:57:34 -0600

    removed link from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RegisterUser from './Components/Auth/RegisterUser'
 import LoginUser from './Components/Auth/LoginUser'
 import LandingPage from './Components/Landing/LandingPage'
