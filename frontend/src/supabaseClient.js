90a760b3f4f590de196c7a3261128f98692e3411
Nick <aurelius@MacBook-Pro.local>
2024-11-20 19:40:51 -0600

    updated supabaseclient

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,8 +1,11 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mklzktwiwxijdbyxpmux.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbHprdHdpd3hpamRieXhwbXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0OTI0MzIsImV4cCI6MjA0NDA2ODQzMn0.qJ7iFsqO272TbYdvP0UMbelWpPdcVoRMYBax6oy01e0'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and anon key are required')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
