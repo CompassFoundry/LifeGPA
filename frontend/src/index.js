c30fb8fb4396144cdc01b872ef7c9ba44e30a7ec
Nick <aurelius@MacBook-Pro.local>
2025-01-03 15:23:08 -0600

    added posthog

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import AuthProvider from '@components/Auth/AuthState'
+import posthog from 'posthog-js'
 
+posthog.init('phc_z4J3egnUkpasUcfgQea8gQaHQPiPG1YVta537i7bJL1', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+})
+
+// Render the React app
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
